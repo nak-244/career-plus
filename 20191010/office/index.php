fc8ff8517d73036ca58f77f6fb1352f5b4c9d379
nak-244 <t09024656860@gmail.com>
2019-10-10 09:51:17 +0900

    up

---
+++ 20191010/office/index.php
@@ -0,0 +1,159 @@
+<?php include ('../header.php'); ?>
+<title>事業所案内 |【公式】株式会社キャリアプラス - 日払い対応人材派遣サービス</title>
+<div id="main">
+<div id="main-all">
+<!--/トップ固定-->
+
+<style type="text/css">
+<!--
+h2{
+text-align: center;
+border-bottom: double 6px #9d0784;
+}
+h3{
+border-bottom: dotted 1px #9d0784;
+}
+/*--SP--*/
+.sp_disp .content-1{
+margin: 1em auto 2em;
+text-align: center;
+}
+.sp_disp .address{
+text-align: left;
+margin:1em 0;
+}
+.sp_disp .map{
+width:100%;
+}
+.sp_disp .map iframe{
+width:100%;
+}
+/*--PC--*/
+.pc_disp .content-1{
+margin: 1em auto 2em;
+text-align: center;
+width:600px;
+}
+.pc_disp .address{
+text-align: left;
+margin:1em 0;
+}
+-->
+</style>
+
+<div class="sp_disp">
+
+<div class="wrap">
+<h2>事業所案内</h2>
+
+<div class="content-1">
+<h3>本社</h3>
+<p class="address">〒160-0023<br>
+東京都新宿区西新宿3-1-3 MITSUWAビル7F<br>
+Tel: 03-5909-0790<br>Fax: 03-5909-0791</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.579843462993!2d139.69518390000005!3d35.68734620000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd234cc59df%3A0xa1a7f9493a2bcae8!2z44CSMTYwLTAwMjMg5p2x5Lqs6YO95paw5a6_5Yy66KW_5paw5a6_77yT5LiB55uu77yR4oiS77yT!5e0!3m2!1sja!2sjp!4v1425628482220" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>札幌支店</h3>
+<p class="address">〒060-0001<br>
+札幌市中央区北1条西3-2 井門札幌ビル9階<br>
+Tel: 011-222-1350<br>Fax: 011-222-1351</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1836.3571513295683!2d141.35131285312153!3d43.061726094753865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b290b0acfc177%3A0x4677ce47ae1ea7e!2z5qCq5byP5Lya56S-44Kt44Oj44Oq44Ki44OX44Op44K5IOacreW5jOaUr-W6lw!5e0!3m2!1sja!2sjp!4v1533004656792" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>新宿支店</h3>
+<p class="address">〒160-0023<br>
+東京都新宿区西新宿3-1-3 MITSUWAビル7F<br>
+Tel: 03-5909-0790<br>Fax: 03-5909-0791</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.579843462993!2d139.69518390000005!3d35.68734620000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd234cc59df%3A0xa1a7f9493a2bcae8!2z44CSMTYwLTAwMjMg5p2x5Lqs6YO95paw5a6_5Yy66KW_5paw5a6_77yT5LiB55uu77yR4oiS77yT!5e0!3m2!1sja!2sjp!4v1425628482220" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>福岡支店</h3>
+<p class="address">〒810-0001<br>
+福岡県福岡市中央区天神2-8-30 天神西通りビジネスセンタービル 8階<br>
+Tel: 092-718-3720<br>Fax: 092-718-3721</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3323.576929658539!2d130.3963714!3d33.5903323!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541918f47a908a7%3A0xf482c9c648e5d772!2z44CSODEwLTAwMDEg56aP5bKh55yM56aP5bKh5biC5Lit5aSu5Yy65aSp56We77yS5LiB55uu77yY4oiS77yT77yQ!5e0!3m2!1sja!2sjp!4v1425628910862" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>沖縄支店</h3>
+<p class="address">〒900-0006<br>
+沖縄県那覇市おもろまち1丁目2番17号 アイワ・プラザ 2階<br>
+Tel: 098-868-3135<br>Fax: 098-868-3136</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3579.1229408050776!2d127.6933242!3d26.225194600000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e56bd84724bd1d%3A0x39c0f1135462ee66!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yS4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1425628965505" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+</div><!--/content-->
+
+</div><!--/sp_disp-->
+
+
+
+<div class="pc_disp">
+
+<div class="wrap">
+<h2>事業所案内</h2>
+
+<div class="content-1">
+<h3>本社</h3>
+<p class="address">〒160-0023<br>
+東京都新宿区西新宿3-1-3 MITSUWAビル7F<br>
+Tel: 03-5909-0790　Fax: 03-5909-0791</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.579843462993!2d139.69518390000005!3d35.68734620000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd234cc59df%3A0xa1a7f9493a2bcae8!2z44CSMTYwLTAwMjMg5p2x5Lqs6YO95paw5a6_5Yy66KW_5paw5a6_77yT5LiB55uu77yR4oiS77yT!5e0!3m2!1sja!2sjp!4v1425628482220" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>札幌支店</h3>
+<p class="address">〒060-0001<br>
+札幌市中央区北1条西3-2 井門札幌ビル9階<br>
+Tel: 011-222-1350　Fax: 011-222-1351</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2915.024391044556!2d141.35099941565375!3d43.061954560747786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b290b0acfc177%3A0x4677ce47ae1ea7e!2z5qCq5byP5Lya56S-44Kt44Oj44Oq44Ki44OX44Op44K5IOacreW5jOaUr-W6lw!5e0!3m2!1sja!2sjp!4v1533004778555" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>新宿支店</h3>
+<p class="address">〒160-0023<br>
+東京都新宿区西新宿3-1-3 MITSUWAビル7F<br>
+Tel: 03-5909-0790　Fax: 03-5909-0791</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.579843462993!2d139.69518390000005!3d35.68734620000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd234cc59df%3A0xa1a7f9493a2bcae8!2z44CSMTYwLTAwMjMg5p2x5Lqs6YO95paw5a6_5Yy66KW_5paw5a6_77yT5LiB55uu77yR4oiS77yT!5e0!3m2!1sja!2sjp!4v1425628482220" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>福岡支店</h3>
+<p class="address">〒810-0001<br>
+福岡県福岡市中央区天神2-8-30 天神西通りビジネスセンタービル 8階<br>
+Tel: 092-718-3720　Fax: 092-718-3721</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3323.576929658539!2d130.3963714!3d33.5903323!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3541918f47a908a7%3A0xf482c9c648e5d772!2z44CSODEwLTAwMDEg56aP5bKh55yM56aP5bKh5biC5Lit5aSu5Yy65aSp56We77yS5LiB55uu77yY4oiS77yT77yQ!5e0!3m2!1sja!2sjp!4v1425628910862" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>沖縄支店</h3>
+<p class="address">〒900-0006<br>
+沖縄県那覇市おもろまち1丁目2番17号 アイワ・プラザ 2階<br>
+Tel: 098-868-3135　Fax: 098-868-3136</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3579.1229408050776!2d127.6933242!3d26.225194600000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e56bd84724bd1d%3A0x39c0f1135462ee66!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yS4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1425628965505" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+</div><!--/content-->
+
+</div><!--/pc_disp-->
+
+
+<!--下部固定/-->
+</div><!--main-all-->
+<?php include ('../side.php'); ?>
+</div><!--main-->
+<?php include ('../footer.php'); ?>
---
+++ 20191010/privacy/index.php
@@ -0,0 +1,553 @@
+<?php include ('../header.php'); ?>
+<title>プライバシーポリシー |【公式】株式会社キャリアプラス - 日払い対応人材派遣サービス</title>
+<div id="main">
+<div id="main-all">
+<!--/トップ固定-->
+
+<style type="text/css">
+<!--
+h3{
+border-bottom: dotted 1px #9d0784;
+margin-bottom:1em;
+font-size: 20px;
+}
+.bold{
+font-weight:bold;
+}
+.violet{
+color: #9d0784;
+}
+.space{
+margin:1em 0;
+}
+/*--SP--*/
+.sp_disp .wrap{
+font-size:15px;
+}
+.sp_disp .wrap p{
+margin:1em 0;
+}
+.sp_disp .wrap li{
+list-style: unset;
+}
+.sp_disp .wrap2 p{
+margin:1em 0;
+}
+.sp_disp .wrap2 li{
+list-style: unset;
+}
+.sp_disp .wrap2{
+font-size:15px;
+margin:2em 0;
+}
+.sp_disp .content{
+margin:1em 0;
+}
+.sp_disp .content2{
+margin:1em 0;
+text-align:right;
+}
+.sp_disp .wrap li,
+.sp_disp .wrap2 li{
+margin:1em 0;
+}
+.sp_disp .kaizi_set{
+margin:0 0 15px 0;
+border:1px solid #ccc;
+border-spacing:1px;
+}
+.sp_disp .kaizi_set th{
+font-size:14px;
+font-weight:bold;
+width:50%;
+padding:5px;
+text-align:center;
+border:1px solid #ccc;
+}
+.sp_disp .kaizi_set td{
+font-size:12px;
+text-align:left;
+padding:3px;
+border:1px solid #ccc;
+}
+.sp_disp .kaizi_set td ul{
+margin:0 0 0 15px;
+}
+.sp_disp .kaizi_set td li{
+list-style:disc;
+margin:0 0 3px 0;
+}
+.sp_disp .syutoku_set{
+width:100%;
+margin:0 0 2px 0;
+border:1px solid #ccc;
+border-spacing:1px;
+}
+.sp_disp .syutoku_set th{
+font-size:14px;
+font-weight:bold;
+width:50%;
+padding:5px;
+text-align:center;
+border:1px solid #ccc;
+}
+.sp_disp .syutoku_set td{
+font-size:12px;
+text-align:left;
+padding:3px;
+border:1px solid #ccc;
+}
+/*--PC--*/
+.pc_disp {
+padding-left: 10px;
+}
+.pc_disp .wrap{
+font-size:15px;
+margin:0 1em;
+}
+.pc_disp .wrap p{
+margin:1em 0;
+}
+.pc_disp .wrap li{
+list-style: unset;
+}
+.pc_disp .wrap2 p{
+margin:1em 0;
+}
+.pc_disp .wrap2 li{
+list-style: unset;
+}
+.pc_disp .wrap2{
+font-size:15px;
+margin:2em 1em;
+}
+.pc_disp .content{
+margin:1em 0;
+}
+.pc_disp .content2{
+margin:1em 0;
+text-align:right;
+}
+.pc_disp .wrap li,
+.pc_disp .wrap2 li{
+margin:1em 0;
+}
+.pc_disp .kaizi_set{
+margin:0 0 15px 0;
+border:1px solid #ccc;
+border-spacing:1px;
+}
+.pc_disp .kaizi_set th{
+font-size:14px;
+font-weight:bold;
+width:50%;
+padding:5px;
+text-align:center;
+border:1px solid #ccc;
+}
+.pc_disp .kaizi_set td{
+font-size:12px;
+text-align:left;
+padding:3px;
+border:1px solid #ccc;
+}
+.pc_disp .kaizi_set td ul{
+margin:0 0 0 15px;
+}
+.pc_disp .kaizi_set td li{
+list-style:disc;
+margin:0 0 3px 0;
+}
+.pc_disp .syutoku_set{
+width:100%;
+margin:0 0 2px 0;
+border:1px solid #ccc;
+border-spacing:1px;
+}
+.pc_disp .syutoku_set th{
+font-size:14px;
+font-weight:bold;
+width:50%;
+padding:5px;
+text-align:center;
+border:1px solid #ccc;
+}
+.pc_disp .syutoku_set td{
+font-size:12px;
+text-align:left;
+padding:3px;
+border:1px solid #ccc;
+}
+-->
+</style>
+
+<div class="sp_disp">
+
+<div class="wrap">
+
+<h3>プライバシーポリシー</h3>
+
+<div class="content">
+<p><a href="https://privacymark.jp/" target="_blank" rel="nofollow"><img src="../img/mark_p.png" alt="Pマーク"></a></p>
+<p class="bold violet">個人情報保護理念</p>
+<p>当社はヒューマンリソーシズ事業、コールセンター受託運営サービス事業を行う個人情報取扱事業者として、お預かりした個人情報の適正な取り扱いおよび保護の徹底は、社会的責務であると考えております。 さらに、個人情報の保護によりお客様にご安心いただくことは、当社の事業活動の重要な基本事項の一つであると考えております。 そのため当社では、「個人情報保護方針」を掲げ、その遂行のために最大限努力することをここに宣言いたします。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">個人情報保護方針</p>
+<ol type="1">
+<li>当社は、事業の内容及び規模を考慮した適切な個人情報の取得、利用及び提供を行います。それには特定された利用目的の達成に必要な範囲を超えた個人情報の取扱いを行わないこと及びそのための措置を講じることを含みます。</li>
+<li>当社は、個人情報の取扱いに関する法令、国が定める指針その他の規範を遵守します。</li>
+<li>当社では、個人情報への不正アクセスまたは個人情報の漏洩、滅失又は棄損などのリスクを予防するために合理的な安全対策を講じ、是正処置を行います。</li>
+<li>当社は、個人情報の保護管理者を選任するなど、個人情報保護に関する管理体制を確立するとともに、当社規程を役員および従業員に周知し、その遵守徹底に努めます。</li>
+<li>当社は最新のＩＴ技術の動向、社会的要請の変化、経営環境の変動等を常に意識しながら、教育、運用、監査などを通じて、適時・適切に個人情報保護マネジメントシステムを見直し、その改善を継続的に行います。</li>
+<li>当社では、個人情報の利用目的の通知、開示、訂正、追加又は削除、利用又は第三者への提供の拒否要求、その他苦情、ご相談など、個人情報に関するお問合せについての窓口を設置し、2週間以内に対応いたします。</li>
+</ol>
+</div><!--/content-->
+
+<div class="content2">
+<p>制定年月日　2013年6月1日<br>
+最終改訂日 2015年1月20日<br>
+株式会社キャリアプラス<br>
+代表取締役　魚山　毅</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="violet">【個人情報に関するお問い合わせ先】</p>
+<p>株式会社キャリアプラス<br>
+Mail：privacyinfo-cap@career-plus.co.jp<br>
+担当：個人情報保護管理者（本社 本部長）</p>
+</div><!--/content-->
+
+</div><!--/wrap-->
+
+<div class="wrap2">
+
+<h3>個人情報の取扱いに関する公表事項</h3>
+<div class="content">
+<p><a href="https://privacymark.jp/" target="_blank" rel="nofollow"><img src="../img/mark_p.png" alt="Pマーク"></a>株式会社キャリアプラス（以下「当社」という）は、「個人情報の保護に関する法律」、「JIS Q 15001:2006」に基づき、以下の事項を公表いたします。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">1.個人情報の利用目的の公表に関する事項</p>
+<p>書面以外で直接個人情報を取得する場合及び間接的に個人情報を取得する場合における利用目的及び当社が保有する個人情報の利用目的は次のとおりです。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="violet">1-1	開示対象の個人情報</p>
+<!--table1-->
+<table class="kaizi_set">
+<tr>
+<th>開示対象個人情報の項目</th>
+<th>開示対象個人情報の利用目的</th>
+</tr>
+<tr>
+<td>当社への採用選考を希望される方に関する個人情報
+（他社求人サイト様から取得した情報、提携先から取得した情報、オープンループ・グループ企業各社から取得した情報を含みます。）</td>
+<td>
+<ul>
+<li>採用選考に関する手続き</li>
+<li>お問合せ内容の確認、回答</li>
+</ul>
+</td>
+<tr>
+<td>社員に関する個人情報</td>
+<td>
+<ul>
+<li>人事管理の手続き</li>
+<li>給与管理の手続き</li>
+<li>雇用保険、社会保険の手続きのため</li>
+<li>お問合せ内容の確認、回答</li>
+</ul>
+</td>
+</tr>
+<tr>
+<td>ヒューマンリソーシズ事業に関する個人情報</td>
+<td>
+<ul>
+<li>登録手続きのため</li>
+<li>お仕事紹介のため</li>
+<li>お仕事情報提供のため</li>
+<li>給与等の管理のため</li>
+<li>福利厚生のご案内のため</li>
+<li>各種セミナーのご案内のため</li>
+</ul>
+</td>
+</tr>
+</table>
+<!--/table1-->
+</div><!--/content-->
+
+<div class="content">
+<p class="violet">1-2	直接書面以外取得の個人情報</p>
+<!--table2-->
+<table class="syutoku_set">
+<tr>
+<th>直接書面以外取得の個人情報の項目</th>
+<th>直接書面以外取得の個人情報の利用目的</th>
+</tr>
+<tr>
+<td>コールセンター受託運営サービス事業により委託された個人情報</td>
+<td>・マーケティング及び各種サポートのため</td>
+</tr>
+<tr>
+<td>他社求人サイトから取得した情報、提携先から取得した情報</td>
+<td>・お仕事紹介のため</td>
+</tr>
+<tr>
+<td>オープンループ・グループ企業各社から取得した情報</td>
+<td>・お仕事紹介のため</td>
+</tr>
+</table>
+<!--/table2-->
+<p>※顧客及びその他の本人から直接書面に記載された個人情報を取得する場合は、その都度、利用目的を明示させて頂きます。それ以外で個人情報を直接取得する場合、又は間接的に取得する場合は、上記の利用目的の制限の範囲内で取扱わせて頂きます。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">2.個人情報の第三者提供について</p>
+<p>当社は、顧客及びその他の本人より取得させていただいた個人情報を適切に管理し、あらかじめ顧客及びその他の本人の同意を得ることなく第三者に提供することはございません。ただし、次の場合は除きます。</p>
+<ol type="a">
+<li>法令に基づく場合</li>
+<li>人の生命、身体又は財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき</li>
+<li>公衆衛生の向上又は児童の健全な育成の推進のために特に必要がある場合であって、本人の同意を得ることが困難であるとき</li>
+<li>国の機関若しくは地方公共団体又はその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって、本人の同意を得ることにより当該事務の遂行に支障を及ぼすおそれがあるとき</li>
+</ol>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">3.開示等の求めに応じる手続き</p>
+<ol type="a">
+<li>開示等の求めの申し出先<br>
+株式会社キャリアプラス<br>
+東京都新宿区西新宿3-1-3　MITSUWAビル 7F<br>
+TEL：03-5909-0790　MAIL：privacyinfo-cap@career-plus.co.jp</li>
+<li>開示等の求めに際して提出すべき書面の様式
+	<ol type="A">
+	<li>概ね過去１年において勤務実績のある登録スタッフの方 開示等の請求を行う場合には、下記Bによる方法の他、お電話、メールにて開示等の請求をすることができます。お電話、メールにて開示等の請求を行う場合には、3（a)開示等の求めの申し出先に氏名、住所、生年月日、連絡先、スタッフコードをご連絡ください。それにより本人確認をさせていただきます。</li>
+	<li>上記A以外の方<br>開示等の請求を行う場合は、当社指定の様式を以下からダウンロードしていただき、所定の事項を全てご記入の上、下記（c）本人確認書類とともにご郵送ください。<a href="../pdf/kaizi_20150216.pdf">「開示等の求め申請書」（PDF）</a></li>
+	</ol>
+</li>
+<li>本人確認書類<br>
+運転免許証、旅券（パスポート）、健康保険証、年金手帳、その他本人を証明するもののコピー。 ※上記本人確認書類の中で「本籍地」が明記されている場合は、「本籍地」を黒く塗りつぶし判読できない状態にしてご郵送ください。</li>
+<li>開示等の求めに対する回答方法<br>
+申請者ご本人に対して、速やかにメール、郵便、電話により回答いたします。 なお、本人確認ができない等、開示等に対応できない場合は、その旨と理由をご連絡いたします。</li>
+<li>開示等の求めにより取得した個人情報の取扱について<br>
+【利用目的】<br>
+ご請求時に頂いた個人情報は、開示等への対応にのみ使用いたします。<br>
+【第三者提供、委託について】<br>
+ご請求時に頂いた個人情報は、第三者に提供、委託することはございません。<br>
+【情報のご提供について】<br>
+当社への個人情報の提供は、すべて任意となっております。<br>
+ただし、当社が依頼する情報の提供がない場合、適正な開示等ができない場合があります。<br>
+【個人情報の返却について】<br>
+本人確認または代理人確認のためにお預かりした個人情報は、対応後直ちに廃棄いたします。</li>
+</ol>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">4.「問い合わせ・苦情」の受付窓口に関する事項</p>
+<p>個人情報に関する問い合わせ・苦情については、下記までお申し出ください。<br>
+株式会社キャリアプラス<br>
+MAIL：privacyinfo-cap@career-plus.co.jp<br>
+担当：個人情報保護管理者（本社 本部長）<br>
+※直接ご来社いただいてのお申し出はお受けいたしかねますので、その旨ご了承賜りますようお願い申し上げます。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">5.認定個人情報保護団体の名称及び、苦情の解決の申し出先について</p>
+<p>当社は、認定個人情報保護団体の対象事業者ではございません。</p>
+</div><!--/content-->
+
+</div><!--/wrap2-->
+
+</div><!--/sp_disp-->
+
+
+
+<div class="pc_disp">
+
+<div class="wrap">
+
+<h3>プライバシーポリシー</h3>
+
+<div class="content">
+<p><a href="https://privacymark.jp/" target="_blank" rel="nofollow"><img src="../img/mark_p.png" alt="Pマーク"></a></p>
+<p class="bold violet">個人情報保護理念</p>
+<p>当社はヒューマンリソーシズ事業、コールセンター受託運営サービス事業を行う個人情報取扱事業者として、お預かりした個人情報の適正な取り扱いおよび保護の徹底は、社会的責務であると考えております。 さらに、個人情報の保護によりお客様にご安心いただくことは、当社の事業活動の重要な基本事項の一つであると考えております。 そのため当社では、「個人情報保護方針」を掲げ、その遂行のために最大限努力することをここに宣言いたします。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">個人情報保護方針</p>
+<ol type="1">
+<li>当社は、事業の内容及び規模を考慮した適切な個人情報の取得、利用及び提供を行います。それには特定された利用目的の達成に必要な範囲を超えた個人情報の取扱いを行わないこと及びそのための措置を講じることを含みます。</li>
+<li>当社は、個人情報の取扱いに関する法令、国が定める指針その他の規範を遵守します。</li>
+<li>当社では、個人情報への不正アクセスまたは個人情報の漏洩、滅失又は棄損などのリスクを予防するために合理的な安全対策を講じ、是正処置を行います。</li>
+<li>当社は、個人情報の保護管理者を選任するなど、個人情報保護に関する管理体制を確立するとともに、当社規程を役員および従業員に周知し、その遵守徹底に努めます。</li>
+<li>当社は最新のＩＴ技術の動向、社会的要請の変化、経営環境の変動等を常に意識しながら、教育、運用、監査などを通じて、適時・適切に個人情報保護マネジメントシステムを見直し、その改善を継続的に行います。</li>
+<li>当社では、個人情報の利用目的の通知、開示、訂正、追加又は削除、利用又は第三者への提供の拒否要求、その他苦情、ご相談など、個人情報に関するお問合せについての窓口を設置し、2週間以内に対応いたします。</li>
+</ol>
+</div><!--/content-->
+
+<div class="content2">
+<p>制定年月日　2013年6月1日<br>
+最終改訂日 2015年1月20日<br>
+株式会社キャリアプラス<br>
+代表取締役　魚山　毅</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="violet">【個人情報に関するお問い合わせ先】</p>
+<p>株式会社キャリアプラス<br>
+Mail：privacyinfo-cap@career-plus.co.jp<br>
+担当：個人情報保護管理者（本社 本部長）</p>
+</div><!--/content-->
+
+</div><!--/wrap-->
+
+<div class="wrap2">
+
+<h3>個人情報の取扱いに関する公表事項</h3>
+<div class="content">
+<p>株式会社キャリアプラス（以下「当社」という）は、「個人情報の保護に関する法律」、「JIS Q 15001:2006」に基づき、以下の事項を公表いたします。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">1.個人情報の利用目的の公表に関する事項</p>
+<p>書面以外で直接個人情報を取得する場合及び間接的に個人情報を取得する場合における利用目的及び当社が保有する個人情報の利用目的は次のとおりです。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="violet">1-1	開示対象の個人情報</p>
+<!--table1-->
+<table class="kaizi_set">
+<tr>
+<th>開示対象個人情報の項目</th>
+<th>開示対象個人情報の利用目的</th>
+</tr>
+<tr>
+<td>当社への採用選考を希望される方に関する個人情報
+（他社求人サイト様から取得した情報、提携先から取得した情報、オープンループ・グループ企業各社から取得した情報を含みます。）</td>
+<td>
+<ul>
+<li>採用選考に関する手続き</li>
+<li>お問合せ内容の確認、回答</li>
+</ul>
+</td>
+<tr>
+<td>社員に関する個人情報</td>
+<td>
+<ul>
+<li>人事管理の手続き</li>
+<li>給与管理の手続き</li>
+<li>雇用保険、社会保険の手続きのため</li>
+<li>お問合せ内容の確認、回答</li>
+</ul>
+</td>
+</tr>
+<tr>
+<td>ヒューマンリソーシズ事業に関する個人情報</td>
+<td>
+<ul>
+<li>登録手続きのため</li>
+<li>お仕事紹介のため</li>
+<li>お仕事情報提供のため</li>
+<li>給与等の管理のため</li>
+<li>福利厚生のご案内のため</li>
+<li>各種セミナーのご案内のため</li>
+</ul>
+</td>
+</tr>
+</table>
+<!--/table1-->
+</div><!--/content-->
+
+<div class="content">
+<p class="violet">1-2	直接書面以外取得の個人情報</p>
+<!--table2-->
+<table class="syutoku_set">
+<tr>
+<th>直接書面以外取得の個人情報の項目</th>
+<th>直接書面以外取得の個人情報の利用目的</th>
+</tr>
+<tr>
+<td>コールセンター受託運営サービス事業により委託された個人情報</td>
+<td>・マーケティング及び各種サポートのため</td>
+</tr>
+<tr>
+<td>他社求人サイトから取得した情報、提携先から取得した情報</td>
+<td>・お仕事紹介のため</td>
+</tr>
+<tr>
+<td>オープンループ・グループ企業各社から取得した情報</td>
+<td>・お仕事紹介のため</td>
+</tr>
+</table>
+<!--/table2-->
+<p>※顧客及びその他の本人から直接書面に記載された個人情報を取得する場合は、その都度、利用目的を明示させて頂きます。それ以外で個人情報を直接取得する場合、又は間接的に取得する場合は、上記の利用目的の制限の範囲内で取扱わせて頂きます。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">2.個人情報の第三者提供について</p>
+<p>当社は、顧客及びその他の本人より取得させていただいた個人情報を適切に管理し、あらかじめ顧客及びその他の本人の同意を得ることなく第三者に提供することはございません。ただし、次の場合は除きます。</p>
+<ol type="a">
+<li>法令に基づく場合</li>
+<li>人の生命、身体又は財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき</li>
+<li>公衆衛生の向上又は児童の健全な育成の推進のために特に必要がある場合であって、本人の同意を得ることが困難であるとき</li>
+<li>国の機関若しくは地方公共団体又はその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって、本人の同意を得ることにより当該事務の遂行に支障を及ぼすおそれがあるとき</li>
+</ol>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">3.開示等の求めに応じる手続き</p>
+<ol type="a">
+<li>開示等の求めの申し出先<br>
+株式会社キャリアプラス<br>
+東京都新宿区西新宿3-1-3　MITSUWAビル 7F<br>
+TEL：03-5909-0790　MAIL：privacyinfo-cap@career-plus.co.jp</li>
+<li>開示等の求めに際して提出すべき書面の様式
+	<ol type="A">
+	<li>概ね過去１年において勤務実績のある登録スタッフの方 開示等の請求を行う場合には、下記Bによる方法の他、お電話、メールにて開示等の請求をすることができます。お電話、メールにて開示等の請求を行う場合には、3（a)開示等の求めの申し出先に氏名、住所、生年月日、連絡先、スタッフコードをご連絡ください。それにより本人確認をさせていただきます。</li>
+	<li>上記A以外の方<br>開示等の請求を行う場合は、当社指定の様式を以下からダウンロードしていただき、所定の事項を全てご記入の上、下記（c）本人確認書類とともにご郵送ください。<a href="../pdf/kaizi_20150216.pdf">「開示等の求め申請書」（PDF）</a></li>
+	</ol>
+</li>
+<li>本人確認書類<br>
+運転免許証、旅券（パスポート）、健康保険証、年金手帳、その他本人を証明するもののコピー。 ※上記本人確認書類の中で「本籍地」が明記されている場合は、「本籍地」を黒く塗りつぶし判読できない状態にしてご郵送ください。</li>
+<li>開示等の求めに対する回答方法<br>
+申請者ご本人に対して、速やかにメール、郵便、電話により回答いたします。 なお、本人確認ができない等、開示等に対応できない場合は、その旨と理由をご連絡いたします。</li>
+<li>開示等の求めにより取得した個人情報の取扱について<br>
+【利用目的】<br>
+ご請求時に頂いた個人情報は、開示等への対応にのみ使用いたします。<br>
+【第三者提供、委託について】<br>
+ご請求時に頂いた個人情報は、第三者に提供、委託することはございません。<br>
+【情報のご提供について】<br>
+当社への個人情報の提供は、すべて任意となっております。<br>
+ただし、当社が依頼する情報の提供がない場合、適正な開示等ができない場合があります。<br>
+【個人情報の返却について】<br>
+本人確認または代理人確認のためにお預かりした個人情報は、対応後直ちに廃棄いたします。</li>
+</ol>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">4.「問い合わせ・苦情」の受付窓口に関する事項</p>
+<p>個人情報に関する問い合わせ・苦情については、下記までお申し出ください。<br>
+株式会社キャリアプラス<br>
+MAIL：privacyinfo-cap@career-plus.co.jp<br>
+担当：個人情報保護管理者（本社 本部長）<br>
+※直接ご来社いただいてのお申し出はお受けいたしかねますので、その旨ご了承賜りますようお願い申し上げます。</p>
+</div><!--/content-->
+
+<div class="content">
+<p class="bold violet">5.認定個人情報保護団体の名称及び、苦情の解決の申し出先について</p>
+<p>当社は、認定個人情報保護団体の対象事業者ではございません。</p>
+</div><!--/content-->
+
+</div><!--/wrap2-->
+
+</div><!--/pc_disp-->
+
+
+<!--下部固定/-->
+</div><!--main-all-->
+<?php include ('../side.php'); ?>
+</div><!--main-->
+<?php include ('../footer.php'); ?>
