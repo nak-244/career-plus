f2ad6cc23375cbc0a3a18f91007a49a32bba64ee
nak-244 <t09024656860@gmail.com>
2021-10-15 14:26:26 +0900

    up

---
+++ docs/company/new3.php
@@ -0,0 +1,282 @@
+<?php include ('../header.php'); ?>
+<title>会社概要 |【公式】株式会社キャリアプラス - 日払い対応人材派遣サービス</title>
+<div id="main">
+<div id="main-all">
+
+<style type="text/css">
+
+h2{
+text-align: center;
+border-bottom: double 6px #9d0784;
+}
+h3{
+border-bottom: dotted 1px #9d0784;
+margin-bottom:1em;
+}
+.bold{
+font-weight:bold;
+}
+.space{
+margin:1em 0;
+}
+/*--SP--*/
+.sp_disp .content-all{
+margin: 1em auto;
+}
+.sp_disp .content{
+text-align:left;
+margin: 1em 0;
+}
+.sp_disp .content-l{
+}
+.sp_disp .content-r{
+}
+.sp_disp .content2{
+}
+.sp_disp .content3{
+text-align:right;
+}
+.sp_disp .wrap2{
+margin:2em auto;
+}
+
+/*--PC--*/
+.pc_disp .content-all{
+width:600px;
+margin: 1em auto;
+}
+.pc_disp .content{
+text-align:left;
+margin: 1em 0;
+}
+.pc_disp .content-l{
+float:left;
+width:20%;
+}
+.pc_disp .content-r{
+float:left;
+width:75%;
+}
+.pc_disp .content2{
+}
+.pc_disp .content3{
+text-align:right;
+}
+.pc_disp .wrap2{
+margin:2em auto;
+width:600px;
+}
+
+</style>
+<!-- スマホ -->
+<div class="sp_disp">
+
+<div class="wrap">
+<h2>会社概要</h2>
+
+<div class="content-all">
+
+<div class="content">
+<div class="content-l">
+<p class="bold">会社名</p>
+</div>
+<div class="content-r">
+<p>株式会社キャリアプラス  Career plus Inc.</p>
+</div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">設立</p>
+</div>
+<div class="content-r">
+<p>2013（平成25）年1月25日</p>
+</div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">事業内容</p>
+</div>
+<div class="content-r">
+<p>ヒューマンリソーシズ事業<br>
+コールセンター受託運営サービス事業</p>
+</div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">代表取締役</p>
+</div>
+<div class="content-r">
+<p>山本　賢一</p>
+</div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">資本金</p>
+</div>
+<div class="content-r">
+<p>1億円（2016年9月現在）</p>
+</div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">許可番号</p>
+</div>
+<div class="content-r">
+<p>労働者派遣事業許可番号　派13-305359<br>
+有料職業紹介事業許可番号   13-ユ-306072</p>
+</div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">取引銀行</p>
+</div>
+<div class="content-r">
+<p>三井住友銀行 新宿支店</p>
+</div>
+</div>
+
+<div class="content">
+<a href="https://privacymark.jp/" target="_blank" rel="nofollow"><img src="../img/mark_p.png" alt="Pマーク"></a>
+</div>
+
+</div>
+
+</div>
+
+
+<div class="wrap2">
+
+<div class="content2">
+<img src="img/ceo3_02.png" width="100%">
+<p class="space">アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは思いました。</p>
+<p class="space">そこでアリスは、頭のなかで、ひなぎくのくさりをつくったら楽しいだろうけれど、起きあがってひなぎくをつむのもめんどくさいし、どうしようかと考えていました（といっても、昼間で暑いし、とってもねむくて頭もまわらなかったので、これもたいへんだったのですが）。</p>
+<p class="space">そこへいきなり、ピンクの目をした白うさぎが近くを走ってきたのです。それだけなら、そんなにめずらしいことでもありませんでした。さらにアリスとしては、そのうさぎが「どうしよう！　どうしよう！　ちこくしちゃうぞ！」とつぶやくのを聞いたときも、それがそんなにへんてこだとは思いませんでした（あとから考えてみたら、これも不思議に思うべきだったのですけれど、でもこのときには、それがごく自然なことに思えたのです）。でもそのうさぎがほんとうに、チョッキのポケットから懐中時計（かいちゅうどけい）をとりだしてそれをながめ、そしてまたあわててかけだしたとき、アリスもとびあがりました。</p>
+</div>
+
+<div class="content3">
+<p>令和元年８月１日</p>
+<img src="img/ykname.png" alt="代表取締役">
+</div>
+
+</div>
+
+</div>
+<!-- /スマホ -->
+
+<!-- PC -->
+<div class="pc_disp">
+
+<div class="wrap">
+<h2>会社概要</h2>
+
+<div class="content-all">
+
+<div class="content">
+<div class="content-l">
+<p class="bold">会社名</p>
+</div>
+<div class="content-r">
+<p>株式会社キャリアプラス  Career plus Inc.</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">設立</p>
+</div>
+<div class="content-r">
+<p>2013（平成25）年1月25日</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">事業内容</p>
+</div>
+<div class="content-r">
+<p>ヒューマンリソーシズ事業<br>
+コールセンター受託運営サービス事業</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">代表取締役</p>
+</div>
+<div class="content-r">
+<p>山本　賢一</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">資本金</p>
+</div>
+<div class="content-r">
+<p>1億円（2016年9月現在）</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">許可番号</p>
+</div>
+<div class="content-r">
+<p>労働者派遣事業許可番号　派13-305359<br>
+有料職業紹介事業許可番号   13-ユ-306072</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<div class="content-l">
+<p class="bold">取引銀行</p>
+</div>
+<div class="content-r">
+<p>三井住友銀行 新宿支店</p>
+</div>
+<div style="clear:both;"></div>
+</div>
+
+<div class="content">
+<a href="https://privacymark.jp/" target="_blank" rel="nofollow"><img src="../img/mark_p.png" alt="Pマーク"></a>
+</div>
+
+</div>
+
+</div>
+
+<div class="wrap2">
+
+<div class="content2">
+<img src="img/ceo3_02.png" width="100%">
+<p class="space">アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは思いました。</p>
+<p class="space">そこでアリスは、頭のなかで、ひなぎくのくさりをつくったら楽しいだろうけれど、起きあがってひなぎくをつむのもめんどくさいし、どうしようかと考えていました（といっても、昼間で暑いし、とってもねむくて頭もまわらなかったので、これもたいへんだったのですが）。</p>
+<p class="space">そこへいきなり、ピンクの目をした白うさぎが近くを走ってきたのです。それだけなら、そんなにめずらしいことでもありませんでした。さらにアリスとしては、そのうさぎが「どうしよう！　どうしよう！　ちこくしちゃうぞ！」とつぶやくのを聞いたときも、それがそんなにへんてこだとは思いませんでした（あとから考えてみたら、これも不思議に思うべきだったのですけれど、でもこのときには、それがごく自然なことに思えたのです）。でもそのうさぎがほんとうに、チョッキのポケットから懐中時計（かいちゅうどけい）をとりだしてそれをながめ、そしてまたあわててかけだしたとき、アリスもとびあがりました。</p>
+</div>
+
+<div class="content3">
+<p>令和元年８月１日</p>
+<img src="img/ykname.png" alt="代表取締役">
+</div>
+
+</div>
+
+</div>
+<!-- /PC -->
+
+</div>
+<?php include ('../side.php'); ?>
+</div>
+<?php include ('../footer.php'); ?>
---
+++ docs/contact/index.php
@@ -0,0 +1,229 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.career-plus.co.jp/contact.html");
+exit();
+?>
+
+<?php include ('../header.php'); ?>
+<title>お問い合わせ |【公式】株式会社キャリアプラス - 日払い対応人材派遣サービス</title>
+<div id="main">
+<div id="main-all">
+<!--/トップ固定-->
+
+<script type="text/javascript">
+<!--
+function nextPage_pc(){
+	rcheck = document.myFormpc.radiobutton[0].checked;
+	if(rcheck){
+		document.location = "http://www.career-plus.co.jp/contact/form2.php";
+		} else {
+		alert("同意されない場合、お問い合わせいただけません。");
+		}
+}
+//-->
+</script>
+
+<script type="text/javascript">
+<!--
+function staffPage_pc(){
+	rcheck = document.myFormpc.radiobutton[0].checked;
+	if(rcheck){
+		document.location = "http://www.career-plus.co.jp/contact/form1.php";
+		} else {
+		alert("同意されない場合、お問い合わせいただけません。");
+		}
+}
+//-->
+</script>
+
+<script type="text/javascript">
+<!--
+function nextPage_sp(){
+	rcheck = document.myFormsp.radiobutton[0].checked;
+	if(rcheck){
+		document.location = "http://www.career-plus.co.jp/contact/form2.php";
+		} else {
+		alert("同意されない場合、お問い合わせいただけません。");
+		}
+}
+//-->
+</script>
+
+<script type="text/javascript">
+<!--
+function staffPage_sp(){
+	rcheck = document.myFormsp.radiobutton[0].checked;
+	if(rcheck){
+		document.location = "http://www.career-plus.co.jp/contact/form1.php";
+		} else {
+		alert("同意されない場合、お問い合わせいただけません。");
+		}
+}
+//-->
+</script>
+
+<style type="text/css">
+<!--
+h2{
+text-align: center;
+border-bottom: double 6px #9d0784;
+}
+.content{
+padding: 1em;
+text-align: center;
+}
+.content a{
+display: inline-block;
+text-decoration: none;
+background-color: #d761c3;
+border-radius: 8px;
+border-bottom: solid 5px #c105a2;
+padding: 1em;
+width:100%;
+height: 7em;
+}
+.bold{
+font-weight: bold;
+font-size: 20px;
+color: #ffdcfa;
+}
+.slide{
+position: relative;
+top: .7em;
+}
+.white{
+color: #1e1e1e;
+font-size: 17px;
+}
+/*--SP--*/
+.sp_disp .content-right{
+margin-top: 1em;
+}
+/*--PC--*/
+.pc_disp .content-left{
+float:left;
+display: inline-block;
+width: 40%;
+margin-left: 2em;
+}
+.pc_disp .content-right{
+float:right;
+display: inline-block;
+width: 40%;
+margin-right: 2em;
+}
+-->
+</style>
+
+<div class="sp_disp">
+<div class="wrap">
+<h2>お問い合わせ</h2>
+<div style="margin: 35px 0; padding: 10px 20px; border: solid 3px #900378;">
+<h3><span>お問い合わせ情報の取り扱いについて</span></h3>
+<h5 style="margin: 5px 0; font-size: 14px;">【利用目的】</h5>
+<p>株式会社キャリアプラス（以下当社）が取得した個人情報は、お問い合わせへの回答以外に使用することはございません。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【第三者提供】</h5>
+<p>当社は、お預かりした個人情報を、法令の規定または司法手続きに基づく要請による場合を除き、第三者に提供することはございません。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【外部委託】</h5>
+<p>当社は、お預かりした個人情報を委託することがあります。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【情報提供の任意性】</h5>
+<p>当社への個人情報の提供は任意となっています。ただし、情報に漏れやミスがある場合、お問い合わせへの適切な回答ができない場合がございます。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【個人情報の開示等】</h5>
+<p>個人情報の利用目的の通知、開示、訂正、追加または削除、利用又は提供の拒否、その他お問い合わせ等については、下記問い合わせ先までご一報ください。<br />
+        個人情報の取り扱いに関して、詳細は<a href="http://www.career-plus.co.jp/privacy/">プライバシーポリシー</a>をご覧ください。</p>
+<h5 style="margin: 7px 0; font-size: 14px;">個人情報に関するお問い合わせ先</h5>
+<p>03-5909-0790<br />
+          <a href="mailto:privacyinfo-cap@career-plus.co.jp">privacyinfo-cap@career-plus.co.jp</a><br />
+          個人情報保護管理者（本社 本部長）</p>
+</p>
+</div>
+<p>      <!-- //box end --></p>
+<p align="center">上記に同意いただける方は【上記の内容に同意します。】ボタンをチェックして<br />
+        お問い合わせ内容入力画面へお進みください。</p>
+<p><!--/pc_disp--></p>
+<form name="myFormsp">
+<table style="margin: 15px auto; border: solid 2px #900378;">
+<tr>
+<td style="vertical-align: middle; text-align: center;">
+	<label><br />
+	<input name="radiobutton" type="radio" value="agree"><br />
+	上記の内容に同意します<br />
+	</label><br />
+	<input name="radiobutton" type="hidden" value="notagree">
+	</td>
+</tr>
+</table>
+
+<div class="content">
+<div class="content-left">
+<p><a onclick="staffPage_sp()" style="cursor:pointer;"><span class="bold">お問い合わせフォーム</span><br><span class="white">・登録スタッフの皆様<br>・お仕事をお探しの方</span></a></p>
+</div>
+<div class="content-right">
+<p><a onclick="nextPage_sp()" style="cursor:pointer;"><span class="bold slide">お問い合わせフォーム</span><br><span class="white slide">・法人のお客様</span></a></p>
+</div>
+<div style="clear:both;"></div>
+</div><!--/content-->
+</form>
+</div><!--/wrap-->
+</div><!--/sp_disp-->
+
+
+
+<div class="pc_disp">
+<div class="wrap">
+<h2>お問い合わせ</h2>
+<div style="margin: 35px 0; padding: 10px 20px; border: solid 3px #900378;">
+<h3><span>お問い合わせ情報の取り扱いについて</span></h3>
+<h5 style="margin: 5px 0; font-size: 14px;">【利用目的】</h5>
+<p>株式会社キャリアプラス（以下当社）が取得した個人情報は、お問い合わせへの回答以外に使用することはございません。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【第三者提供】</h5>
+<p>当社は、お預かりした個人情報を、法令の規定または司法手続きに基づく要請による場合を除き、第三者に提供することはございません。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【外部委託】</h5>
+<p>当社は、お預かりした個人情報を委託することがあります。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【情報提供の任意性】</h5>
+<p>当社への個人情報の提供は任意となっています。ただし、情報に漏れやミスがある場合、お問い合わせへの適切な回答ができない場合がございます。</p>
+<h5 style="margin: 5px 0; font-size: 14px;">【個人情報の開示等】</h5>
+<p>個人情報の利用目的の通知、開示、訂正、追加または削除、利用又は提供の拒否、その他お問い合わせ等については、下記問い合わせ先までご一報ください。<br />
+        個人情報の取り扱いに関して、詳細は<a href="http://www.career-plus.co.jp/privacy/">プライバシーポリシー</a>をご覧ください。</p>
+<h5 style="margin: 7px 0; font-size: 14px;">個人情報に関するお問い合わせ先</h5>
+<p>03-5909-0790<br />
+          <a href="mailto:privacyinfo-cap@career-plus.co.jp">privacyinfo-cap@career-plus.co.jp</a><br />
+          個人情報保護管理者（本社 本部長）</p>
+</p>
+</div>
+<p>      <!-- //box end --></p>
+<p align="center">上記に同意いただける方は【上記の内容に同意します。】ボタンをチェックして<br />
+        お問い合わせ内容入力画面へお進みください。</p>
+<p><!--/pc_disp--></p>
+<form name="myFormpc">
+<table style="width: 500px; margin: 15px auto; border: solid 2px #900378;">
+<tr>
+<td style="vertical-align: middle; text-align: center;">
+	<label><br />
+	<input name="radiobutton" type="radio" value="agree"><br />
+	上記の内容に同意します<br />
+	</label><br />
+	<input name="radiobutton" type="hidden" value="notagree">
+	</td>
+</tr>
+</table>
+
+<div class="content">
+<div class="content-left">
+<p><a onclick="staffPage_pc()" style="cursor:pointer;"><span class="bold">お問い合わせフォーム</span><br><span class="white">・登録スタッフの皆様<br>・お仕事をお探しの方</span></a></p>
+</div>
+<div class="content-right">
+<p><a onclick="nextPage_pc()" style="cursor:pointer;"><span class="bold slide">お問い合わせフォーム</span><br><span class="white slide">・法人のお客様</span></a></p>
+</div>
+<div style="clear:both;"></div>
+</div><!--/content-->
+</form>
+</div><!--/wrap-->
+</div><!--/pc_disp-->
+
+
+<!--下部固定/-->
+</div><!--main-all-->
+<?php include ('../side.php'); ?>
+</div><!--main-->
+<?php include ('../footer.php'); ?>
