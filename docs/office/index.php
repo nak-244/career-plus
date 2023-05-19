74239c1529c336a8c39336f39a16581ad8a275b6
nak-244 <t09024656860@gmail.com>
2023-05-19 13:03:42 +0900

    up

---
+++ docs/office/index.php
@@ -0,0 +1,184 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.career-plus.co.jp/office.html");
+exit();
+?>
+
+<?php include ('../header.php'); ?>
+<title>事業所案内 |【公式】株式会社キャリアプラス - 日払い対応人材派遣サービス</title>
+<div id="main">
+<div id="main-all">
+<!--/トップ固定-->
+
+<style type="text/css">
+
+h2{
+text-align: center;
+border-bottom: double 6px #9d0784;
+}
+h3{
+border-bottom: dotted 1px #9d0784;
+}
+/*--SP--*/
+.sp_disp .content-1{
+margin: 1em auto 2em;
+text-align: center;
+}
+.sp_disp .address{
+text-align: left;
+margin:1em 0;
+}
+.sp_disp .map{
+width:100%;
+}
+.sp_disp .map iframe{
+width:100%;
+}
+/*--PC--*/
+.pc_disp .content-1{
+margin: 1em auto 2em;
+text-align: center;
+width:600px;
+}
+.pc_disp .address{
+text-align: left;
+margin:1em 0;
+}
+
+</style>
+
+<div class="sp_disp">
+
+<div class="wrap">
+<h2>事業所案内</h2>
+
+<div class="content-1">
+<h3>本社</h3>
+<p class="address">〒160-0022<br>
+東京都新宿区新宿4-3-17 FORECAST新宿SOUTH7階<br>
+Tel: 03-6682-4660<br>Fax: 03-6682-4661</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.5301231223048!2d139.70171771477933!3d35.68857013717779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cdad546963f%3A0x698ab428d3a6e8f5!2z44CSMTYwLTAwMjIg5p2x5Lqs6YO95paw5a6_5Yy65paw5a6_77yU5LiB55uu77yT4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1594434063687!5m2!1sja!2sjp" frameborder="0" style="border:0"></iframe>
+
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>札幌支店</h3>
+<p class="address">〒060-0001<br>
+札幌市中央区北1条西3-2 井門札幌ビル9階<br>
+Tel: 011-222-1350<br>Fax: 011-222-1351</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1836.3571513295683!2d141.35131285312153!3d43.061726094753865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b290b0acfc177%3A0x4677ce47ae1ea7e!2z5qCq5byP5Lya56S-44Kt44Oj44Oq44Ki44OX44Op44K5IOacreW5jOaUr-W6lw!5e0!3m2!1sja!2sjp!4v1533004656792" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>新宿支店</h3>
+<p class="address">〒160-0022<br>
+  東京都新宿区新宿4-3-17 FORECAST新宿SOUTH7階<br>
+  Tel: 03-6682-4660<br>Fax: 03-6682-4661</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.5301231223048!2d139.70171771477933!3d35.68857013717779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cdad546963f%3A0x698ab428d3a6e8f5!2z44CSMTYwLTAwMjIg5p2x5Lqs6YO95paw5a6_5Yy65paw5a6_77yU5LiB55uu77yT4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1594434063687!5m2!1sja!2sjp" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>大阪支店</h3>
+<p class="address">〒541-0053<br>
+大阪府大阪市中央区本町4丁目4-17 RE-012 5F<br>
+Tel: 06-6258-8630<br>Fax: 06-6258-8631</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.877994733713!2d135.4964013152318!3d34.68302858043912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e6e29d2883a7%3A0xeb1d78a3788134ae!2z44CSNTQxLTAwNTMg5aSn6Ziq5bqc5aSn6Ziq5biC5Lit5aSu5Yy65pys55S677yU5LiB55uu77yU4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1575508199861!5m2!1sja!2sjp" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>福岡支店</h3>
+<p class="address">〒812-0011<br>
+福岡県福岡市博多区博多駅前3-2-8 住友生命博多ビル6F<br>
+Tel: 092-432-6760<br>Fax: 092-432-6761</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.673731211768!2d130.416068115203!3d33.58781958073499!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x354191b8fe61f2bb%3A0x6d0419c35b6057c9!2z44CSODEyLTAwMTEg56aP5bKh55yM56aP5bKh5biC5Y2a5aSa5Yy65Y2a5aSa6aeF5YmN77yT5LiB55uu77yS4oiS77yY!5e0!3m2!1sja!2sjp!4v1570669148834!5m2!1sja!2sjp" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>沖縄支店</h3>
+<p class="address">〒900-0006<br>
+沖縄県那覇市おもろまち1丁目2番17号 アイワ・プラザ 2階<br>
+Tel: 098-868-3135<br>Fax: 098-868-3136</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3579.1229408050776!2d127.6933242!3d26.225194600000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e56bd84724bd1d%3A0x39c0f1135462ee66!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yS4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1425628965505" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+</div><!--/content-->
+
+</div><!--/sp_disp-->
+
+
+
+<div class="pc_disp">
+
+<div class="wrap">
+<h2>事業所案内</h2>
+
+<div class="content-1">
+<h3>本社</h3>
+<p class="address">〒160-0022<br>
+東京都新宿区新宿4-3-17 FORECAST新宿SOUTH7階<br>
+Tel: 03-6682-4660<br>Fax: 03-6682-4661</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.5301231223048!2d139.70171771477933!3d35.68857013717779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cdad546963f%3A0x698ab428d3a6e8f5!2z44CSMTYwLTAwMjIg5p2x5Lqs6YO95paw5a6_5Yy65paw5a6_77yU5LiB55uu77yT4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1594434063687!5m2!1sja!2sjp" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>札幌支店</h3>
+<p class="address">〒060-0001<br>
+札幌市中央区北1条西3-2 井門札幌ビル9階<br>
+Tel: 011-222-1350　Fax: 011-222-1351</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2915.024391044556!2d141.35099941565375!3d43.061954560747786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b290b0acfc177%3A0x4677ce47ae1ea7e!2z5qCq5byP5Lya56S-44Kt44Oj44Oq44Ki44OX44Op44K5IOacreW5jOaUr-W6lw!5e0!3m2!1sja!2sjp!4v1533004778555" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>新宿支店</h3>
+<p class="address">〒160-0022<br>
+東京都新宿区新宿4-3-17 FORECAST新宿SOUTH7階<br>
+Tel: 03-6682-4660<br>Fax: 03-6682-4661</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.5301231223048!2d139.70171771477933!3d35.68857013717779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cdad546963f%3A0x698ab428d3a6e8f5!2z44CSMTYwLTAwMjIg5p2x5Lqs6YO95paw5a6_5Yy65paw5a6_77yU5LiB55uu77yT4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1594434063687!5m2!1sja!2sjp" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>大阪支店</h3>
+<p class="address">〒541-0053<br>
+大阪府大阪市中央区本町4丁目4-17 RE-012 5F<br>
+Tel: 06-6258-8630　Fax: 06-6258-8631</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.877994733713!2d135.4964013152318!3d34.68302858043912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e6e29d2883a7%3A0xeb1d78a3788134ae!2z44CSNTQxLTAwNTMg5aSn6Ziq5bqc5aSn6Ziq5biC5Lit5aSu5Yy65pys55S677yU5LiB55uu77yU4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1575508199861!5m2!1sja!2sjp" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>福岡支店</h3>
+<p class="address">〒812-0011<br>
+福岡県福岡市博多区博多駅前3-2-8 住友生命博多ビル6F<br>
+Tel: 092-432-6760　Fax: 092-432-6761</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.673731211768!2d130.416068115203!3d33.58781958073499!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x354191b8fe61f2bb%3A0x6d0419c35b6057c9!2z44CSODEyLTAwMTEg56aP5bKh55yM56aP5bKh5biC5Y2a5aSa5Yy65Y2a5aSa6aeF5YmN77yT5LiB55uu77yS4oiS77yY!5e0!3m2!1sja!2sjp!4v1570669249212!5m2!1sja!2sjp" width="600" height="250" frameborder="0" style="border:0;"></iframe>
+</div>
+</div><!--/content1-->
+
+<div class="content-1">
+<h3>沖縄支店</h3>
+<p class="address">〒900-0006<br>
+沖縄県那覇市おもろまち1丁目2番17号 アイワ・プラザ 2階<br>
+Tel: 098-868-3135　Fax: 098-868-3136</p>
+<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3579.1229408050776!2d127.6933242!3d26.225194600000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e56bd84724bd1d%3A0x39c0f1135462ee66!2z44CSOTAwLTAwMDYg5rKW57iE55yM6YKj6KaH5biC44GK44KC44KN44G-44Gh77yR5LiB55uu77yS4oiS77yR77yX!5e0!3m2!1sja!2sjp!4v1425628965505" width="600" height="250" frameborder="0" style="border:0"></iframe>
+</div>
+</div><!--/content1-->
+
+</div><!--/content-->
+
+</div><!--/pc_disp-->
+
+
+<!--下部固定/-->
+</div><!--main-all-->
+<?php include ('../side.php'); ?>
+</div><!--main-->
+<?php include ('../footer.php'); ?>
